3f998d177a88cbce1c902962a67e06a65ee037e4
Kasumba Raymond <kasumbaraymond0756519001@gmail.com>
2022-10-17 00:10:34 +0300

    Correction

--- 1-digit.c
+++ 1-digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * init_params - clears struct fields and reset buf
+ * init_params - clears the struct fields and reset buf
  * @params: the parameters struct
  * @ap: the argument pointer
  * Return: void
